aa3bb85de1ea35929deab40204683897c0c5acbb
donkeyrat <darklordbananas@gmail.com>
2022-11-20 13:32:17 -0800

    Added destroy support

--- Kill.cs
+++ Kill.cs
@@ -8,7 +8,10 @@ namespace HiddenUnits
     {
         public void KillYourself()
         {
-            transform.root.GetComponent<Unit>().data.healthHandler.Die();
+            if (transform.root.GetComponent<Unit>()) transform.root.GetComponent<Unit>().data.healthHandler.Die();
+            if (destroy) transform.root.GetComponent<Unit>().DestroyUnit();
         }
+
+        public bool destroy;
     }
 }
