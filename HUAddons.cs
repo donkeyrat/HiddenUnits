1ab6f8c24a5e144e4b8fd7c527eede07bb7ee57b
donkeyrat <darklordbananas@gmail.com>
2022-11-20 20:44:23 -0800

    New HUAddons, helps with things here and there

---
+++ HUAddons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace HiddenUnits
+{
+    public static class HUAddons
+    {
+        public static object GetField(Type type, object instance, string fieldName)
+        {
+            BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            FieldInfo field = type.GetField(fieldName, bindingAttr);
+            return field.GetValue(instance);
+        }
+
+        public static void SetField<T>(object originalObject, string fieldName, T newValue)
+        {
+            BindingFlags bindingAttr = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            FieldInfo field = originalObject.GetType().GetField(fieldName, bindingAttr);
+            field.SetValue(originalObject, newValue);
+        }
+    }
+}
